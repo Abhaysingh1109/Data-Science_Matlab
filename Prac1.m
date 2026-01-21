a06fb0c5469ad02c320f281d9d0f2d1127eebbbf
Abhaysingh1109 <abhaysingh11091999@gmail.com>
2026-01-21 01:08:50 +0100

    Add some mathematical practice using exercise.

---
+++ Prac1.m
@@ -0,0 +1,32 @@
+%% Exercise 1.1
+
+% Row vector
+on = [0 1 2];
+
+% Column vector
+vn = [0; 1; 2];
+
+% Matrix (2 rows, 3 columns)
+mn = [0 1 2;
+      3 4 5];
+
+% Row vector containing odd numbers from 1 to 15
+nd = 1:2:15;
+
+disp(nd)
+disp(on);
+disp(vn);
+disp(mn);
+
+
+%% Exercise 1.2
+
+% Row string array
+ot = ["John" "Doe"];
+
+% Column string array
+vt = ["John";
+      "Doe"];
+
+disp(vt)
+disp(ot)
